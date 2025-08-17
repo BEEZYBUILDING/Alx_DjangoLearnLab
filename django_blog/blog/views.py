483312a6ca841943f9deea4d8de48b446ab08618
OLAJIRE <olajirefolasore@gmail.com>
2025-08-17 12:03:04 +0100

    add

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")  # redirect after successful registration
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "register.html", {"form": form})
