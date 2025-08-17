e814e22a463e53a889810505030adacc2de82779
OLAJIRE <olajirefolasore@gmail.com>
2025-08-17 12:28:24 +0100

    add

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,9 +14,12 @@ urlpatterns = [
     path("logout/", auth_views.LogoutView.as_view(template_name="logout.html"), name="logout"),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
-     path("posts/", PostListView.as_view(), name="post-list"),
+    path("posts/", PostListView.as_view(), name="post-list"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("posts/new/", PostCreateView.as_view(), name="post-create"),
-    path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post-update"),
+    path("posts/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
     path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
-]
+    
+    
+    
+
