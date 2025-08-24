bf287b5b4e0766902da056aa9e02ca1fad467093
OLAJIRE <olajirefolasore@gmail.com>
2025-08-24 22:03:24 +0100

    updated posts

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('feed/', FeedView.as_view(), name='feed'),
     path('posts/<int:pk>/like/', LikePostView.as_view(), name='like-post'),
     path('posts/<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
+    path('feed/', FeedView.as_view(), name='user-feed'),
 ] + router.urls
