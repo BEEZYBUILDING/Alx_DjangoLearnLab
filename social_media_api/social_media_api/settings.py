4972ed384cda2024f9e6817406c706e886739044
OLAJIRE <olajirefolasore@gmail.com>
2025-08-24 21:42:20 +0100

    add

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -130,7 +130,10 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
-    ];
+    ]
+}
+
+REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,   # adjust as needed
     'DEFAULT_FILTER_BACKENDS': [
